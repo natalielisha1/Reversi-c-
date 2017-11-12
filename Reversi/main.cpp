3e297eff5191d2d761b7c4aa609129c60504c622
Natalie Elisha <natalie7399@gmail.com>
2017-11-12 14:28:50 +0200

    Adding mainHelp.h

--- Reversi/main.cpp
+++ Reversi/main.cpp
@@ -1,10 +1,12 @@
 
 
 #include <iostream>
+#include "mainHelp.h"
 using namespace std;
 
 int main() {
 	cout << "This is a c++ placeholder" << endl;
 	cout << "Making a change to main.cpp" << endl;
+	cout << main2() << endl;
 	return 0;
 }
---
+++ Reversi/mainHelp.h
@@ -0,0 +1,17 @@
+/*
+ * mainHelp.h
+ *
+ *  Created on: 12 בנוב׳ 2017
+ *      Author: natalie
+ */
+
+#ifndef MAINHELP_H_
+#define MAINHELP_H_
+
+
+int main2() {
+	return 2;
+}
+
+
+#endif /* MAINHELP_H_ */
